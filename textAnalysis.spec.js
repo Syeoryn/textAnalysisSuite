cb8c70bd2ea072906a734168d44a342df5a32e8f
Drew Cuthbertson <syeoryn@gmail.com>
2014-11-09 00:18:12 -0800

    Fixes addTags spec

--- textAnalysis.spec.js
+++ textAnalysis.spec.js
@@ -83,6 +83,6 @@ describe('The addTags method', function(){
     var text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     var analysis = textAnalysis.analyzeText(text);
     textAnalysis.addTags(analysis, ['test']);
-    expect(textAnalysis.tags).to.deep.equal(['test']);
+    expect(analysis.tags).to.deep.equal(['test']);
   });
 });
